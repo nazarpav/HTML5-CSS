9e1959e66c73321e77baf0125a5caa22868a41e9
nazar pavliuk <hesab.k18@gmail.com>
2020-06-11 02:11:01 +0300

    homework

---
+++ Homework/11_06_2020/ConsoleSenderEmail/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleSenderEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int action=0;
+            do
+            {
+                Console.WriteLine("0. Exit\n1. Send to Email, email pattern");
+                Console.Write("->_");
+                action = int.Parse(Console.ReadLine());
+                switch (action)
+                {
+                    case 1:
+                        {
+                            string folder = "email";
+                            string fileName = "index.html";
+                            string pathFile = Path.Combine(folder,fileName);
+                            string html = File.ReadAllText(pathFile);
+                            using (SMTPSender sender = new SMTPSender())
+                            {
+                                Console.WriteLine("Enter your email: ");
+                                string email = Console.ReadLine();
+                                //novakvova@gmail.com
+                                sender.SendMessage(email, html);
+                            }
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            } while (action != 0); ;
+        }
+    }
+}
---
+++ Homework/11_06_2020/ConsoleSenderEmail/SMTPSender.cs
@@ -0,0 +1,44 @@
+﻿using EASendMail;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleSenderEmail
+{
+    class SMTPSender : IDisposable
+    {
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+        public bool SendMessage(string email, string html)
+        {
+            try
+            {
+                SmtpServer server = new SmtpServer("smtp.gmail.com")
+                {
+                    Port = 587,
+                    ConnectType = SmtpConnectType.ConnectSSLAuto,
+                    User = "itstudentyre@gmail.com",
+                    Password = "Qw3eI98*63%"
+                };
+                SmtpMail message = new SmtpMail("TryIt")
+                {
+                    From = server.User,
+                    To = email,
+                    Subject = "Verification code with Win Viewer",
+                    //TextBody = html,
+                    Priority = MailPriority.High,
+                    HtmlBody=html
+                };
+                SmtpClient client = new SmtpClient();
+                client.SendMail(server, message);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
